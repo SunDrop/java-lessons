9b0afa6d03055c102783175fba81c13963d84b4e
Slava Konashkov <slava.konashkov@gmail.com>
2014-03-24 20:49:59 +0200

    Update FileEvent.java

--- FileMonitor/FileEvent.java
+++ FileMonitor/FileEvent.java
@@ -4,6 +4,6 @@ public class FileEvent implements IFileEvent {
 	@Override
 	public void onFileAdded(File file) {
 		System.out.println("File added: " + file.getName());
-        System.out.println("File date created: " + new java.util.Date((long)file.lastModified()));
+        	System.out.println("File date created: " + new java.util.Date((long)file.lastModified()));
 	}
 }
