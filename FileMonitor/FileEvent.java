8aa7a1394a51913266df5419a7bd2e787a0627e9
Slava Konashkov <slava.konashkov@gmail.com>
2014-03-24 20:47:44 +0200

    Create FileEvent.java

---
+++ FileMonitor/FileEvent.java
@@ -0,0 +1,9 @@
+import java.io.File;
+
+public class FileEvent implements IFileEvent {
+	@Override
+	public void onFileAdded(File file) {
+		System.out.println("File added: " + file.getName());
+        System.out.println("File date created: " + new java.util.Date((long)file.lastModified()));
+	}
+}
