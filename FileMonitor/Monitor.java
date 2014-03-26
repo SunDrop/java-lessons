5b095c16f8b86e42cc75f17cc03c6788e2a0f198
Slava Konashkov <slava.konashkov@gmail.com>
2014-03-26 21:15:53 +0200

    Update Monitor.java

--- FileMonitor/Monitor.java
+++ FileMonitor/Monitor.java
@@ -1,38 +1,100 @@
 import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
 
 public class Monitor {
+
     IFileEvent event;
-    String[] files = new String[10];
+    String[] filesMonitor = new String[10];
+    String[] filesExclude = new String[100];
     int iSize = 0;
+    int iExcludeSize = 0;
 
 
     public Monitor(IFileEvent event) {
         this.event = event;
     }
 
+    public Monitor addDir(String sName) throws Exception {
+        if (!(new File(sName)).isDirectory()) {
+            throw new Exception("You add not directory");
+        }
+        return this.addFile(sName);
+    }
+
     public Monitor addFile(String sName) throws Exception {
-        if (iSize >= files.length) {
+        if (iSize >= filesMonitor.length) {
             throw new Exception("MaxFileMonitorLimit");
         }
-        files[iSize++] = sName;
+        filesMonitor[iSize++] = sName;
         return this;
     }
 
+    public Monitor removeFile(int iIndex) throws Exception {
+        if (iIndex >= filesMonitor.length) {
+            throw new Exception("Index delete error");
+        }
+        System.arraycopy(filesMonitor, iIndex + 1, filesMonitor, iIndex, --iSize - iIndex);
+        return this;
+    }
+
+    public boolean isExclude(String sName) {
+        return Arrays.asList(filesExclude).contains(sName);
+    }
+
+    public void addExcludeFile(String sName) throws Exception {
+        if (iExcludeSize >= filesExclude.length) {
+            throw new Exception("MaxFileExcludeLimit");
+        }
+        filesExclude[iExcludeSize++] = sName;
+    }
+
     public void start() {
-        while (true) {
+        while (iSize > 0) {
             for (int i = 0; i < iSize; i++) {
-                File f = new File(files[i]);
-                if (f.exists() && f.isFile()) {
-                    if (event != null) {
-                        event.onFileAdded(f);
+                File f = new File(filesMonitor[i]);
+                if (f.exists()) {
+                    if (f.isFile()) {
+                        if (event != null) {
+                            event.onFileAdded(f);
+                        }
+                        try {
+                            this.removeFile(i);
+                        } catch (Exception e) {}
+                    } else if (f.isDirectory()) {
+                        File[] files = f.listFiles(new MyFileFilter(".txt"));
+
+                        for (File file: files) {
+                            try {
+                                String sName = file.getCanonicalPath();
+                                if (!isExclude(sName)) {
+                                    if (event != null) {
+                                        event.onFileAdded(file);
+                                    }
+                                    addExcludeFile(sName);
+                                }
+                            } catch (Exception e) {}
+                        }
                     }
-                    return;
                 }
                 try {
                     Thread.sleep(1000);
-                } catch (InterruptedException e) {}
+                } catch (InterruptedException e) {
+                }
             }
             System.out.println("Waiting...");
+        }
+    }
+
+    static class MyFileFilter implements FilenameFilter {
+        private String ext;
+
+        public MyFileFilter(String ext) {
+            this.ext = ext.toLowerCase();
+        }
+
+        public boolean accept(File dir, String name) {
+            return name.toLowerCase().endsWith(this.ext);
         }
     }
 }
